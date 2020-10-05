47121e69084b08d0b6b8515af1acfff5cd8e74d2
Rafsan Jany, 170042044 <rafsanjany@iut-dhaka.edu>
2020-10-06 03:18:09 +0600

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import React from 'react'
-import {NavigationContainer} from "@react-navigation/native"
-import {createStackNavigator} from "@react-navigation/stack"
-import HomeScreen from "./src/screens/HomeScreen"
-import ListScreen from "./src/screens/ListScreen"
-const stack = createStackNavigator()
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import Home from './src/screens/Home';
+import Profile from './src/screens/Profile';
+import sem1 from './src/screens/sem1';
+import sem2 from './src/screens/sem2';
+import sem3 from './src/screens/sem3';
+import FacultyList from './src/screens/FacultyList';
+import Semesters from './src/screens/Semesters';
 
-const HiRafsan = () => {
-    return (
-      <NavigationContainer>
-        <stack.Navigator initialRouteName="Home">
-          <stack.Screen name="Home" component={HomeScreen}/>
 
-          <stack.Screen name="list" component={ListScreen}/>
-          </stack.Navigator>
-      </NavigationContainer>
+const stack = createStackNavigator();
 
-    )};
+function App(){
+  return(
+    <NavigationContainer>
+      <stack.Navigator initialRouteName="Home">
+        <stack.Screen name="Home" component= {Home} />
+        <stack.Screen name="Profile" component= {Profile}/>
+        <stack.Screen name="FacultyList" component= {FacultyList}/>
+        <stack.Screen name="Semesters" component= {Semesters}/>
+        <stack.Screen name="Semester_1" component= {sem1}/>
+        <stack.Screen name="Semester_2" component= {sem2}/>
+        <stack.Screen name="Semester_3" component= {sem3}/>
+      </stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
-export default HiRafsan;
+export default App;
