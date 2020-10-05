fb6e4671f7cfcd081f8a2f7da95f8298fbb2858a
Rafsan Jany, 170042044 <rafsanjany@iut-dhaka.edu>
2020-10-05 20:23:50 +0600

    #1

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View,StyleSheet,Text } from 'react-native';
+import  {Statusbar} from "expo-status-bar"
+
+const HiRafsan = () => {
+    return (
+      <View style={style.container}>
+      <Text>open letas somthing the </Text>
+      </View>
+    );
+};
+
+const style = StyleSheet.create({
+  container:{flex: 1, backgroundColor:"purple",alignItems:"center",justifyContent:"center"},
+})
+
+export default HiRafsan;
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import React from "react"
+import {Text,StyleSheet} from "react-native"
+const HomeScreen=()=>{
+    return(<Text styles={styles.textStyle}>This the home page</Text>)
+}
+
+const styles =StyleSheet.create({
+    textStyle:{
+        fontSize:30,
+        color: "bule",
+    }
+})
+
+export default HomeScreen
