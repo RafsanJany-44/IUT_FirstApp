05589508dc927ebf81e5b78ed631cbd68a0ea8a4
Rafsan Jany, 170042044 <rafsanjany@iut-dhaka.edu>
2020-10-05 21:00:33 +0600

    createStackNavigator fixed

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import {NavigationContainer} from "@react-navigation/native"
-import {creatStackNavigator} from "@react-navigation/stack"
+import {createStackNavigator} from "@react-navigation/stack"
 import HomeScreen from "./src/screens/HomeScreen"
-const stack = creatStackNavigator()
+const stack = createStackNavigator()
 
 const HiRafsan = () => {
     return (
